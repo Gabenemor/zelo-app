1ac11d2da3d095bbb278b388bdb2636cdc7d6a4d
Gabriel Enemor <winterstaark@gmail.com>
2025-06-09 15:58:39 +0000

    give a 5 percent margin on both sides fot eh website on desktop

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
         <link href="https://fonts.googleapis.com/css2?family=PT+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet" />
         <link href="https://fonts.googleapis.com/css2?family=Source+Code+Pro:ital,wght@0,200..0,900;1,200..1,900&display=swap" rel="stylesheet" />
       </head>
-      <body className="font-body antialiased px-[5%] sm:px-0">
+      <body className="font-body antialiased px-[5%] sm:px-0 lg:px-[5%]">
         {children}
         <Toaster />
       </body>
