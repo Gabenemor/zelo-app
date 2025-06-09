ab17dd7546419a77daba7ca266499df8141da1dd
Gabriel Enemor <winterstaark@gmail.com>
2025-06-09 15:52:04 +0000

    make it 90 percent, also when i ope it on a new tab it does not rspect t

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -10,7 +10,7 @@ export default function DashboardLayout({
   return (
     <div className="flex min-h-screen flex-col">
       <DashboardHeader />
-      <main className="flex-1 p-4 sm:p-6 bg-background w-full lg:w-[80%] mx-auto">
+      <main className="flex-1 p-4 sm:p-6 bg-background w-full lg:w-[90%] mx-auto">
         {children}
       </main>
       <Toaster /> {/* Ensure Toaster is available if sub-components use toasts */}
